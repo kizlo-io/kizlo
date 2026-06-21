08859ffc54e5d665baf58fdfdaa131ca1cd72753
IDJGILL <inderjeetgill.me@gmail.com>
2026-06-21 10:49:25 +0530

    feat(web): add docs/blog/home site with namespaced route groups

---
+++ web/src/app/(blog)/blog/page.tsx
@@ -0,0 +1,7 @@
+export default function BlogPage() {
+	return (
+		<div className="flex flex-1 flex-col justify-center text-center">
+			<p>Coming soon.</p>
+		</div>
+	)
+}
---
+++ web/src/lib/shared.ts
@@ -0,0 +1,10 @@
+export const appName = "Kizlo"
+export const docsRoute = "/docs"
+export const docsImageRoute = "/og/docs"
+export const docsContentRoute = "/llms.mdx/docs"
+
+export const gitConfig = {
+	user: "kizlo-io",
+	repo: "kizlo",
+	branch: "main",
+}
