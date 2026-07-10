5fe9a2875c265b5818ff0d824b92620bc5be3476
IDJGILL <inderjeetgill.me@gmail.com>
2026-07-11 04:44:43 +0530

    remove force dynamic from robots

--- web/src/app/robots.txt/route.ts
+++ web/src/app/robots.txt/route.ts
@@ -1,6 +1,4 @@
 import { createRobotsRoute } from "kizlo/nextjs/server"
 import { client } from "@/lib/kizlo/server"
 
-export const dynamic = "force-dynamic"
-
 export const GET = createRobotsRoute(client)
