2e64cb94af16c5e0c3afecf49a1b7bf8283cbbe3
IDJGILL <inderjeetgill.me@gmail.com>
2026-07-11 18:29:14 +0530

    next native robots.txt

--- web/src/app/robots.txt/route.ts
+++
@@ -1,6 +0,0 @@
-import { createRobotsRoute } from "kizlo/nextjs/server"
-import { client } from "@/lib/kizlo/server"
-
-export const dynamic = "force-static"
-
-export const GET = createRobotsRoute(client)
